4dc4212fc26a438ac485812d8e853fb32d910b3f
Aryman Garcia Reis <arymanreis@gmail.com>
2025-09-22 21:02:28 -0300

    Primeiro commit para salvar o repositório material didático SQL: Módulo 1 / Dia 1 e 6

---
+++ DADOS/01_DADOS_OPERACIONAIS/01_insercao_dados.sql
@@ -0,0 +1,35 @@
+USE mercado_cafe_db;
+
+-- Inserindo os primeiros registros na tabela Fazendas
+INSERT INTO Fazendas 
+  ( Nome_Fazenda, Municipio, Estado, Area_Total_Hectares, Altitude_Metros )
+VALUES
+  ('Fazenda Monte Alegre', 'Alfenas', 'MG', 1250.50, 1100),
+  ('Sítio da Torre', 'Venda Nova do Imigrante', 'ES', 85.00, 950),
+  ('Chácara Santo Antônio', 'Caconde', 'SP', 120.75, 1250),
+  ('Fazenda Primavera', 'Piatã', 'BA', 550.00, 1300),
+  ('Fazenda Santa Inês', 'Carmo de Minas', 'MG', 215.00, 1200);
+
+-- Inserindo registros na tabela Lotes_Producao
+-- Note como usamos o ID_Fazenda para ligar cada lote à sua fazenda correspondente.
+INSERT INTO Lotes_Producao 
+  (ID_Fazenda, Ano_Safra, Especie_Cafe, Classificacao_Cafe, Volume_Producao_Sacas)
+VALUES
+  (1, 2024, 'Arábica', 'Especial', 350.75),  
+  (2, 2024, 'Conilon', 'Tradicional', 210.00), 
+  (5, 2025, 'Arábica', 'Especial', 280.00),    
+  (3, 2024, 'Arábica', 'Gourmet', 180.50),    
+  (4, 2024, 'Arábica', 'Exótico', 120.25),   
+  (1, 2025, 'Arábica', 'Gourmet', 410.00);
+
+-- Inserindo registros na tabela Vendas
+-- O ID_Lote conecta a venda ao lote de produção específico.
+-- Note que podemos ter várias vendas para o mesmo lote.
+INSERT INTO Vendas (ID_Lote, Data_Venda, Quantidade_Vendida_Sacas, Preco_Por_Saca_BRL, Tipo_Mercado) VALUES
+(1, '2024-10-25', 150.00, 850.50, 'Externo'),    
+(3, '2024-11-05', 180.50, 920.00, 'Externo'),   
+(2, '2024-11-12', 100.00, 780.00, 'Interno'),    
+(2, '2024-11-20', 110.00, 795.50, 'Interno'),    
+(5, '2025-09-15', 280.00, 950.00, 'Externo');   
+
+SELECT * FROM Vendas;
---
+++ DADOS/01_DADOS_OPERACIONAIS/setup_db.sql
@@ -0,0 +1,69 @@
+-- Criar base de dados
+CREATE DATABASE IF NOT EXISTS mercado_cafe_db;
+-- Selecionar o banco de dados criado
+USE mercado_cafe_db;
+
+-- Criando as tabelas
+-- Tabela Fazenda
+CREATE TABLE Fazendas (
+  ID_Fazenda INT PRIMARY KEY AUTO_INCREMENT, 
+  Nome_Fazenda VARCHAR(100) NOT NULL,
+  Municipio VARCHAR(100) NOT NULL,
+  Estado VARCHAR(2) NOT NULL,
+  Area_Total_Hectares DECIMAL (10, 2),
+  Altitude_Metros INT
+);
+
+-- Tabela Lotes produção
+CREATE TABLE Lotes_producao (
+  ID_Lote INT PRIMARY KEY AUTO_INCREMENT,
+  ID_Fazenda INT NOT NULL,
+  Ano_Safra INT NOT NULL,
+  Especie_Cafe VARCHAR(100) NOT NULL,
+  Classificacao_Cafe VARCHAR(100) NOT NULL,
+  Volume_Producao_Sacas INT,
+
+  -- Regra da Foreign Key completa
+  CONSTRAINT FK_Lotes_Fazendas
+    FOREIGN KEY (ID_Fazenda)
+    REFERENCES Fazendas (ID_Fazenda)
+);
+
+-- Altera o nome da tabela
+ALTER TABLE Lotes_producao RENAME TO Lotes_Producao;
+
+-- Alterar o tipo de dado
+ALTER TABLE Lotes_Producao
+MODIFY COLUMN Volume_Producao_Sacas DECIMAL(10, 2);
+
+CREATE TABLE Vendas (
+  ID_Venda INT PRIMARY KEY AUTO_INCREMENT,
+  ID_Lote INT NOT NULL,
+  Data_Venda DATE NOT NULL,
+  Quantidade_Vendida_Sacas INT NOT NULL,
+  Preco_Por_Saca_BRL INT NOT NULL,
+  Tipo_Mercado VARCHAR(10),
+
+  -- Regra da Foreign Key completa
+  CONSTRAINT Fk_Vendas_Lotes
+    FOREIGN KEY (ID_Lote)
+    REFERENCES Lotes_Producao (ID_Lote)
+);
+
+-- Apagar Tabelas
+DROP TABLE Vendas;
+DROP TABLE Lotes_Producao;
+DROP TABLE Fazendas;
+
+SHOW TABLES;
+DESCRIBE Lotes_Producao;
+
+
+
+
+
+
+
+
+
+
