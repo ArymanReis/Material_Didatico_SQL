ad9518977a7df08cfa1fd87bc14c2205d4a6cbbb
Aryman Garcia Reis <arymanreis@gmail.com>
2025-10-23 21:05:53 -0300

    Conclusão módulo 5 dias 61 e 62

---
+++ ANALISES/Modulo_5/dia_61_consultas.sql
@@ -0,0 +1,12 @@
+-- A Pergunta de Negócio: "Preciso de um relatório que mostre cada venda (ID_Venda), a data (Data_Venda), o preço por saca (Preco_Por_Saca_BRL) e, ao lado, o preço por saca da venda imediatamente anterior (ordenado por data)."
+SELECT
+  ID_Venda,
+  Data_Venda,
+  Preco_Por_Saca_BRL,
+  LAG(Preco_Por_Saca_BRL, 1) OVER(ORDER BY Data_Venda ASC) AS "Preço Venda Anterior"
+FROM
+  Vendas
+ORDER BY
+  Data_Venda ASC;
+
+
---
+++ ANALISES/Modulo_5/dia_62_consultas.sql
@@ -0,0 +1,20 @@
+-- Variação percentual do LAG()
+SELECT
+    ID_Venda,
+    Data_Venda,
+    Preco_Por_Saca_BRL,
+    LAG(Preco_Por_Saca_BRL, 1) OVER(ORDER BY Data_Venda ASC) AS "Preço Venda Anterior", -- Mantendo o LAG original para clareza
+    -- Cálculo da variação percentual: ((Atual - Anterior) / Anterior) * 100
+    CONCAT(
+      CEILING(
+        (
+            (Preco_Por_Saca_BRL - LAG(Preco_Por_Saca_BRL, 1) OVER(ORDER BY Data_Venda ASC)) -- (Atual - Anterior)
+            / 
+            NULLIF(LAG(Preco_Por_Saca_BRL, 1) OVER(ORDER BY Data_Venda ASC), 0) -- / NULLIF(Anterior, 0) para evitar divisão por zero/NULL
+        ) * 100
+      ), '%'
+    ) AS "% Variação vs Anterior" -- * 100 e o AS no final
+FROM
+    Vendas
+ORDER BY
+    Data_Venda ASC;
