d6400ec32b1d906b81a5466724b32dce287e2a5a
Aryman Garcia Reis <arymanreis@gmail.com>
2025-12-08 21:08:10 -0300

    Atualizando apostila / Enriquecendo a consulta para abranger todos os conceitos citados

--- ANALISES/Modulo_4/dia_37_consultas.sql
+++ ANALISES/Modulo_4/dia_37_consultas.sql
@@ -1,15 +1,14 @@
 USE mercado_cafe_db;
 
--- Cenário: O time de marketing precisa de uma lista de "tags" ou "etiquetas" para cada lote de produção, para usar em campanhas. A etiqueta precisa ser um texto único, padronizado, todo em maiúsculas e no formato ESPECIE - CLASSIFICACAO (ANO). Além disso, foi detectado que em alguns lotes, a classificação "Especial" foi inserida com o erro de digitação "Spcial".
-
+/* Gerando Etiquetas: Limpeza, Correção e Formatação */
 SELECT
   UPPER(
     CONCAT(
-      Especie_Cafe,
+      TRIM(Especie_Cafe),
       ' - ',
-      REPLACE(Classificacao_Cafe, 'Spcial', 'Especial'),
+      TRIM(REPLACE(Classificacao_Cafe, 'Spcial', 'Especial')),
       ' (', Ano_Safra, ')'
     ) 
-  ) AS "Etiqueta de Marketing"
+  ) AS "Etiqueta_Marketing"
 FROM
   Lotes_Producao;
